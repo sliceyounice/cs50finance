3cfff3f0b282e90db9a59fcd59d0ce94cb1ec097
Igor Prilepov <igor.prilepov98@gmail.com>
2019-06-14 14:23:24 +0300

    buying and selling from portfolio page

--- static/script.js
+++ static/script.js
@@ -1,7 +1,20 @@
-function sendFormData (route, symbol, shares) {
+
+function sendFormData(route, element) {
     let xhr = new XMLHttpRequest();
-    let body = 'symbol='+encodeURIComponent(symbol)+'&shares='+encodeURIComponent(shares);
+    let symbol = element.closest('tr').getAttribute('datatype');
+    let shares = document.getElementById('shares').value;
+    let body = 'symbol=' + encodeURIComponent(symbol) + '&shares=' + encodeURIComponent(shares);
     xhr.open("POST", route, true);
     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     xhr.send(body);
+    xhr.onload = function () {
+        location.reload();
+    }
 }
+
+document.querySelector('#buy').addEventListener('click', function () {
+    sendFormData('/buy', this);
+});
+document.querySelector('#sell').addEventListener('click', function () {
+    sendFormData('/sell', this);
+});
